a5e4955b5a661e5bdb3fdbc8372e87eb8e092b2b
Raphael Ponce <raphafleury123@gmail.com>
2020-01-28 01:32:47 -0300

    Primeiro commit

---
+++ Assets/Scripts/AreaExit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AreaExit : MonoBehaviour
+{
+    public string areaToLoad;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(areaToLoad);
+        }
+    }
+}
+
+
+
+
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D playerRB;
+    public Animator playerMoveAnim;
+
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Update()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+
+        playerRB.velocity = new Vector2(horizontal, vertical) * moveSpeed;
+
+        playerMoveAnim.SetFloat("moveX", playerRB.velocity.x);
+        playerMoveAnim.SetFloat("moveY", playerRB.velocity.y);
+        handleAnimation(horizontal, vertical);
+    }
+
+    private void handleAnimation(float horizontal, float vertical)
+    {
+        if (horizontal == 1 || horizontal == -1 || vertical == 1 || vertical == -1)
+        {
+            playerMoveAnim.SetFloat("lastMoveX", horizontal);
+            playerMoveAnim.SetFloat("lastMoveY", vertical);
+        }
+    }
+}
