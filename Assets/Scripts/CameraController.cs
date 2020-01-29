7e12a1269a1393eb5596d038f5990f7b482cee39
Raphael Ponce <raphafleury123@gmail.com>
2020-01-29 01:50:13 -0300

    Fade entre cenas

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,17 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraController : MonoBehaviour
 {
     public Transform target;
+    public Tilemap tilemap;
+    private Vector3 bottomLeftLimit;
+    private Vector3 topRightLimit;
+
+    private float halfHeight;
+    private float halfWidth;
     void Start()
     {
         target = PlayerController.instance.transform;
+
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Camera.main.aspect;
+
+        bottomLeftLimit = tilemap.localBounds.min + new Vector3(halfWidth,halfHeight,0f);
+        topRightLimit = tilemap.localBounds.max + new Vector3(-halfWidth,-halfHeight,0f);
+
+        PlayerController.instance.SetBounds(tilemap.localBounds.min,tilemap.localBounds.max);
     }
 
     void LateUpdate()
     {
-        this.transform.position = new Vector3(target.position.x, target.position.y,this.transform.position.z);
+        this.transform.position = new Vector3(target.position.x, target.position.y, this.transform.position.z);
+
+        //keep the camera inside the bounds
+        this.transform.position = new Vector3(Mathf.Clamp(this.transform.position.x, bottomLeftLimit.x, topRightLimit.x), Mathf.Clamp(this.transform.position.y,bottomLeftLimit.y,topRightLimit.y),this.transform.position.z); 
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     public Animator playerMoveAnim;
     public static PlayerController instance;
     public string areaTransitionName;
+    private Vector3 bottomLeftLimit;
+    private Vector3 topRightLimit;
 
     void Start()
     {
@@ -34,6 +36,15 @@ public class PlayerController : MonoBehaviour
         playerMoveAnim.SetFloat("moveX", playerRB.velocity.x);
         playerMoveAnim.SetFloat("moveY", playerRB.velocity.y);
         handleAnimation(horizontal, vertical);
+
+        this.transform.position = new Vector3(Mathf.Clamp(this.transform.position.x, bottomLeftLimit.x, topRightLimit.x), Mathf.Clamp(this.transform.position.y, bottomLeftLimit.y, topRightLimit.y), this.transform.position.z);
+
+    }
+
+    public void SetBounds(Vector3 botLeft, Vector3 topRight)
+    {
+        bottomLeftLimit = botLeft + new Vector3(0.5f,1f,0f);
+        topRightLimit = topRight + new Vector3(-0.5f,-1f,0f);
     }
 
     private void handleAnimation(float horizontal, float vertical)
